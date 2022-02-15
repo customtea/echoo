a275171b8c1b48edf3ba2a0bd17b222146bb29f1
customtea <kon.taku8@gmail.com>
2022-02-15 14:49:48 +0900

    fix; cmd parser 'n' else missing

--- src/main.rs
+++ src/main.rs
@@ -115,6 +115,8 @@ fn cmd_parser(param: &mut TextColorParam, cmd_buf: &Vec<char>){
             'n' => if !is_prefix && !is_prefix_end{
                 is_prefix = true;
                 is_style = Some(false);
+            }else{
+                cmd.push(*c);
             }
             '_' => if is_prefix{
                 is_prefix = true;
@@ -128,6 +130,7 @@ fn cmd_parser(param: &mut TextColorParam, cmd_buf: &Vec<char>){
         }
     }
     let cmd = cmd.iter().collect::<String>();
+    //println!("{}", cmd);
     match is_frontcolor {
         Some(front) => if front {
             match cmd.as_str() {
